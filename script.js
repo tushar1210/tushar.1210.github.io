165ad327ed54ca384c95ab501fd258c03b2057f5
tushar1210 <tushar.singh@grofers.com>
2024-02-09 03:28:50 +0530

    add logs

--- script.js
+++ script.js
@@ -12,3 +12,51 @@ $(document).ready(function(){
     $('.container').removeClass('open');
   });
 });
+
+
+function onClickYes() {
+    console.log("yes");
+    // window.location.href ='yes.html'
+    var y = document.getElementById("no")
+    y.style.display = "none";
+    var x = document.getElementById("yes")
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    setTimeout(()=>{
+        fadeOutEffect(x)
+    }, 5000)
+}
+
+
+function onClickNo() {
+    // console.log("no");
+    // window.location.href ='no.html'
+    var y = document.getElementById("yes")
+    y.style.display = "none";
+    var x = document.getElementById("no")
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+    setTimeout(()=>{
+        fadeOutEffect(x)
+    }, 5000)
+}
+
+function fadeOutEffect(fadeTarget) {
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity > 0) {
+            fadeTarget.style.opacity -= 0.1;
+        } else {
+            clearInterval(fadeEffect);
+        }
+    }, 200);
+}
